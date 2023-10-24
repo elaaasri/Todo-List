265166b0aa5be443c528756883d053878848d6c6
elaaasri <anas_elasri@outlook.fr>
2023-10-24 19:19:26 +0100

    add default project form

--- src/ui.js
+++ src/ui.js
@@ -21,95 +21,195 @@ function createTaskContainer() {
       <span id="project-title"">${addDefaultProject().name}</span>
       <button id="add-task-button">+ Add Task</button>
   `;
-  editTasks();
+  showTaskForm();
 }
 // event call for default project name :
 const defaultProjectDiv = document.getElementById("default-project-div");
 defaultProjectDiv.addEventListener("click", createTaskContainer);
 
-// // func to create project task :
-function createFormTask() {
-  const projectTaskContainer = document.getElementById(
-    "project-task-container"
-  );
-  const formTask = document.createElement("form");
-  formTask.id = "form-task";
-  formTask.setAttribute("method", "dialog");
-  formTask.innerHTML = `
-          <div id="form-title">
-            <label>Title:</label>
-            <input type="text" placeholder="What To Do:" required />
-          </div>
-          <div id="form-details">
-            <label>Details(optional):</label>
-            <input placeholder="Description:" />
-          </div>
-          <div id="form-date">
-            <label>Date:</label>
-            <input type="date" required />
-          </div>
-          <div id="form-priority">
-            <label>Priority(optional): </label>
-            <div id="form-priority-buttons" required>
-              <input type="button" value="Low" />
-              <input type="button" value="Medium" />
-              <input type="button" value="High" />
-            </div>
-          </div>
-          <div id="form-submit">
-            <input type="submit" value="Add" />
-            <input type="button" value="cancel" />
-          </div>`;
-  projectTaskContainer.appendChild(formTask);
+// func to show task form :
+function showTaskForm() {
+  const addTaskButton = document.getElementById("add-task-button");
+  addTaskButton.addEventListener("click", () => {
+    const formTask = document.getElementById("form-task");
+    formTask.style.cssText = "display: flex";
+  });
 }
-// event call for default project tasks :
-function editTasks() {
-  const defaultTaskButton = document.getElementById("add-task-button");
-  if (defaultTaskButton === null) return;
-  defaultTaskButton.addEventListener("click", createFormTask);
-}
-// event call for form submit :
-// function getFormTasks() {
-//   const submitButton = document.querySelector(
-//     'input[type="submit"][value="Add"]'
-//   );
 
-//   console.log(submitButton);
+// form submit event for showing the the form output  :
+const submitButton = document.querySelector('input[type="submit"]');
+submitButton.addEventListener("click", displayFormOutput);
+
+function displayFormOutput() {
+  // form elements  :
+  let formTask = document.getElementById("form-task");
+  let formTitle = formTask[0];
+  let formDetails = formTask[1];
+  let formDate = formTask[2];
+  if (
+    formTitle.value === "" ||
+    formDetails.value === "" ||
+    formDate.value === ""
+  )
+    return;
+  // display form outputs :
+  const createFormOutputElement = () => {
+    const projectTaskContainer = document.getElementById(
+      "project-task-container"
+    );
+    const taskOutputContainer = document.createElement("div");
+    taskOutputContainer.id = "task-output-container";
+    taskOutputContainer.innerHTML = `
+      <input type="checkbox" />
+      <div id="task-output-tilte">${formTitle.value}</div>
+      <div id="task-output-details">${formDetails.value}</div>
+      <div id="task-output-date">${formDate.value}</div>`;
+    projectTaskContainer.appendChild(taskOutputContainer);
+  };
+  createFormOutputElement();
+  // clean form :
+  const cleanFormData = () => {
+    formTask.reset();
+  };
+  cleanFormData();
+}
+
+// function displayFormOutput() {
+//   const getFormOutput = () => {
+//     const formTask = document.getElementById("form-task");
+//     const formTitleOutput = formTask[0].value;
+//     const formDetailsOutput = formTask[1].value;
+//     const formDateOutput = formTask[2].value;
+//     if (
+//       formTitleOutput === "" ||
+//       formDetailsOutput === "" ||
+//       formDateOutput === ""
+//     )
+//       return;
+//     // formTask.style.cssText = "display: none";
+//   };
+//   getFormOutput();
+//   const createFormOutputElement = () => {
+//     const projectTaskContainer = document.getElementById(
+//       "project-task-container"
+//     );
+//     const taskOutputContainer = document.createElement("div");
+//     taskOutputContainer.id = "task-output-container";
+//     // taskOutputContainer.style.cssText = "display: flex";
+//     taskOutputContainer.innerHTML = `
+//     <div id="task-output-container" style="display: none"></div>
+//     <input type="checkbox" />
+//     <div id="task-output-tilte">${formTitleOutput}</div>
+//     <div id="task-output-details">${formDetailsOutput}</div>
+//     <div id="task-output-date">${formDateOutput}</div>
+//     <div id="task-output-priority">
+//       <button>test</button>
+//     </div>
+//     </div>`;
+//     projectTaskContainer.appendChild(taskOutputContainer);
+//   };
+//   createFormOutputElement();
 // }
 
-const testButton = document.getElementById("test");
+// function getFormOutput() {
+//   const formTask = document.getElementById("form-task");
+//   const formTitleOutput = formTask[0].value;
+//   const formDetailsOutput = formTask[1].value;
+//   const formDateOutput = formTask[2].value;
 
-function testFunc() {
-  const submitButton = document.querySelector(
-    'input[type="submit"][value="Add"]'
-  );
-  console.log(submitButton);
-}
-testButton.addEventListener("click", testFunc);
+//   console.log(formTask);
+//   console.log(formTitleOutput);
+//   console.log(formDetailsOutput);
+//   console.log(formDateOutput);
+// }
 
-// <!-- <form id="form-task">
-// <div id="form-title">
-//   <label>Title:</label>
-//   <input type="text" placeholder="What To Do:" autofocus required />
-// </div>
-// <div id="form-details">
-//   <label>Details(optional):</label>
-//   <input placeholder="Description:" />
-// </div>
-// <div id="form-date">
-//   <label>Date:</label>
-//   <input type="date" required />
-// </div>
-// <div id="form-priority">
-//   <label>Priority: </label>
-//   <div id="form-priority-buttons">
-//     <input type="button" value="Low" />
-//     <input type="button" value="Medium" />
-//     <input type="button" value="High" />
+// function getFormData() {
+//   const formTask = document.getElementById("form-task");
+//   const formTitleOutput = formTask[0].value;
+//   const formDetailsOutput = formTask[1].value;
+//   const formDateOutput = formTask[2].value;
+
+//   // const priorityButtons = document.querySelectorAll(".priority-buttons");
+//   // let chosenPriorityButton;
+//   // priorityButtons.forEach((button) => {
+//   //   button.addEventListener("click", function () {
+//   //     chosenPriorityButton = this;
+//   //   });
+//   // });
+
+//   console.log(formTitleOutput);
+//   console.log(formDetailsOutput);
+//   console.log(formDateOutput);
+//   return {
+//     formTitleOutput,
+//     formDetailsOutput,
+//     formDateOutput,
+//   };
+// }
+//
+// // func to create project task :
+// function createFormTask() {
+//   const projectTaskContainer = document.getElementById(
+//     "project-task-container"
+//   );
+//   const formTask = document.createElement("form");
+//   formTask.id = "form-task";
+//   formTask.setAttribute("method", "dialog");
+//   formTask.innerHTML = `
+//           <div id="form-title">
+//             <label>Title:</label>
+//             <input type="text" placeholder="What To Do:" required />
+//           </div>
+//           <div id="form-details">
+//             <label>Details(optional):</label>
+//             <input placeholder="Description:" />
+//           </div>
+//           <div id="form-date">
+//             <label>Date:</label>
+//             <input type="date" required />
+//           </div>
+//           <div id="form-priority">
+//             <label>Priority(optional): </label>
+//             <div id="form-priority-buttons" required>
+//               <input class="priority-buttons" type="button" value="Low" />
+//               <input class="priority-buttons" type="button" value="Medium" />
+//               <input class="priority-buttons" type="button" value="High" />
+//             </div>
+//           </div>
+//           <div id="form-submit">
+//             <input type="submit" value="Add" />
+//             <input type="button" value="cancel" />
+//           </div>`;
+//   projectTaskContainer.appendChild(formTask);
+//   createFormOutput();
+// }
+// event call for default project tasks :
+// function editTasks() {
+//   const defaultTaskButton = document.getElementById("add-task-button");
+//   if (defaultTaskButton === null) return;
+//   defaultTaskButton.addEventListener("click", createFormTask);
+// }
+
+// function createTaskOutputElement() {
+//   const title = getFormOutput().formTitleOutput;
+//   const details = getFormOutput().formDetailsOutput;
+//   const date = getFormOutput().formDateOutput;
+
+//   const projectTaskContainer = document.getElementById(
+//     "project-task-container"
+//   );
+//   const taskOutputContainer = document.createElement("div");
+//   taskOutputContainer.id = "task-output-container";
+//   // taskOutputContainer.style.cssText = "display: flex";
+//   taskOutputContainer.innerHTML = `
+//   <div id="task-output-container" style="display: none"></div>
+//   <input type="checkbox" />
+//   <div id="task-output-tilte">${title}</div>
+//   <div id="task-output-details">${details}</div>
+//   <div id="task-output-date">${date}</div>
+//   <div id="task-output-priority">
+//     <button>test</button>
 //   </div>
-// </div>
-// <div id="form-submit">
-//   <input type="submit" value="Add" />
-//   <input type="button" value="cancel" />
-// </div>
-// </form> -->
+//   </div>`;
+//   projectTaskContainer.appendChild(taskOutputContainer);
+// }
